71ba1fb2ef3bfe62e5d403ab8ad5678c70f21642
marktnoonan <mark@cypress.io>
2025-05-01 14:56:06 -0400

    add some new elements for demo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,6 +6,8 @@ import CypressLogo from "../components/SvgCypressLogo";
 export default function Footer() {
   return (
     <Container maxWidth="sm" style={{ marginTop: 50 }}>
+      <button data-test="important-button">I'm new and interactive</button>
+      <a href="/a-previously-unknown-page">Link to a 404</a>
       <Typography variant="body2" color="textSecondary" align="center">
         Built by
         <a
