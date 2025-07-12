e7fcf9da2042bcffe207eec2fecf9c31b56acae4
krishna1s <50358529+krishna1s@users.noreply.github.com>
2025-07-12 20:27:29 +0530

    updating playwrigth config

--- playwright.config.ts
+++ playwright.config.ts
@@ -53,7 +53,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "yarn start:ci",
+    command: "yarn run dev",
     url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
     timeout: 120 * 1000,
