b55e079061438c249b769ff7fd3a6422073f5a66
krishna1s <50358529+krishna1s@users.noreply.github.com>
2025-07-14 17:19:17 +0530

    making bug

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://cypress-realworld-app-demo.eastus.azurecontainer.io:3001",
+    baseURL: "http://localhost:3000",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "off",
@@ -66,11 +66,11 @@ export default defineConfig({
     },
   ],
 
-  // /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: "yarn run dev",
-  //   url: "http://cypress-realworld-app-demo.eastus.azurecontainer.io:3001/",
-  //   reuseExistingServer: true,
-  //   timeout: 120 * 1000,
-  // },
+  /* Run your local dev server before starting the tests */
+  webServer: {
+    command: "yarn run dev",
+    url: "http://cypress-realworld-app-demo.eastus.azurecontainer.io:3001/",
+    reuseExistingServer: true,
+    timeout: 120 * 1000,
+  },
 });
