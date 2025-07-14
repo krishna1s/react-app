03a8760993c80b2034ce21d09ea0d30d885e77f7
krishna1s <50358529+krishna1s@users.noreply.github.com>
2025-07-14 15:43:21 +0530

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
     ["junit", { outputFile: "playwright-report/results.xml" }],
   ],
   /* Exit with error if any test fails or is flaky */
-  globalTeardown: process.env.CI ? "./playwright/global-teardown.ts" : undefined,
+  globalTeardown: process.env.CI,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
